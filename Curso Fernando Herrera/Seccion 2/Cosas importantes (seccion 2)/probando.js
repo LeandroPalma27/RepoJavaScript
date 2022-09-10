944482538d0cbea04312b9b97ecdb9f149810a1e
Leandro Palma <psychomusicbox@gmail.com>
2022-09-09 22:16:40 -0500

    Avances

---
+++ Curso Fernando Herrera/Seccion 2/Cosas importantes (seccion 2)/probando.js
@@ -0,0 +1,13 @@
+let saludo = "Hola leandro";
+
+console.log({saludo})
+
+const prueba = [1, 2, 3, 4, 5];
+console.log(prueba.splice(1, 1));
+console.log(prueba)
+
+console.log(Object.entries(prueba))
+
+const remake = prueba.unshift("xd")
+console.log(prueba)
+console.log(remake)
